606d0accfdea33d0f2ea09cb045a6970debf3643
boanergepro <antonycarrizo96@gmail.com>
2017-10-18 10:45:58 -0400

    [MOD] Refactorización del codigo a ES6

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+// ES6
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
 
-var mensajes = [{usuario: "", mensaje: ""}];
+let mensajes = [
+	{
+		usuario: "",
+		mensaje: ""
+	}
+]
 
-// se especifica la carpeta donde estan los archivos estaticos
-app.use(express.static('public'));
+app.use(express.static('public'))
+
 
 // cuando alguien se conecta a la socket (la pagina este abierta)
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
 
 	// se emite el evento mensajes con el arreglo mensajes
-	socket.emit('mensajes', mensajes);
+	socket.emit('mensajes', mensajes)
 
 	// se escucha el evento enviado por el cliente
-	socket.on("nuevoMensaje", function(datos){
-		mensajes.push(datos);
+	socket.on('nuevoMensaje', (data) => {
+		mensajes.push(data)
 
 		// se emite el evento a todos los clientes conectados al socket
-		io.sockets.emit('mensajes', mensajes);
-	});
+		io.sockets.emit('mensajes', mensajes)
+	})
+})
 
-});
-
-// se inicia el servidor
-server.listen(3000, function(){
-	console.log("Servidor corriendo en http://localhost:3000");
-});
-
-// ES6
-// server.listen(3000, () => console.log('servidor corriendo...'));
+//Run server
+server.listen(3000, () => {
+	console.log("Server run port 3000")
+})
